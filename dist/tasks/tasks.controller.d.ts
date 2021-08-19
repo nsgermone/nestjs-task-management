8d98012e78f14fa0d7a0f6113b9e9c8d8d640a30
ngermone <ngermone@simtlix.com>
2021-08-19 17:53:24 -0300

    Define the task model

--- dist/tasks/tasks.controller.d.ts
+++ dist/tasks/tasks.controller.d.ts
@@ -1,6 +1,7 @@
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 export declare class TasksController {
     private tasksService;
     constructor(tasksService: TasksService);
-    getAllTasks(): any[];
+    getAllTasks(): Task[];
 }
--- src/tasks/task.model.ts
+++ src/tasks/task.model.ts
@@ -1,4 +1,4 @@
-export interface Tast {
+export interface Task {
   id: string;
   title: string;
   description: string;
